59ce7836bb6e53bb81dbf4c073c79c3d8e56dd52
Dmitriyjd <dimon05091996abcd@mail.ru>
2018-12-18 17:03:39 +0300

    fixed responcedata getColumnsRequest

--- server/controllers/columns.js
+++ server/controllers/columns.js
@@ -2,12 +2,7 @@ const Columns = require("../dao/columns");
 
 function getColumnsByBoardsId(req,res) {
     Columns.getColumnsByBoardId(req.body.id,(gotColumnsErrors, gotColumns)=>{
-        if(gotColumns.length === 0){
-            res.status(404).json({ errors:['Columns not found'] })
-        }
-        else{
-            res.status(200).json({ columns: gotColumns })
-        }
+        res.status(200).json({ columns: gotColumns })
     })
 }
 
