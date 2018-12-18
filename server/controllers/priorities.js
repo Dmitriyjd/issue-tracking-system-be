2aa4352e8e7f30af33f30c44a304fe7d620f7cc4
Dmitriyjd <dimon05091996abcd@mail.ru>
2018-12-18 17:20:50 +0300

    fixed responcedata priority

--- server/controllers/priorities.js
+++ server/controllers/priorities.js
@@ -2,12 +2,7 @@ const Priorities = require('../dao/priorities');
 
 function getPriorities (req,res){
         Priorities.getPriorities( (gotPrioritiesErrors, gotPriorities)=>{
-            if(gotPriorities.length === 0){
-                res.status(404).json({ errors:['Priorities not found'] })
-            }
-            else{
-                res.status(200).json({ priorities: gotPriorities })
-            }
+            res.status(200).json({ priorities: gotPriorities })
         })
 }
 
